4172df3ae621c3d0e3dd34f616301d1e145a8b60
Marwa <marwama3bade@gmail.com>
2021-11-15 12:56:52 +0000

    Check if return false if one seat is not available RED

--- test/cinema.test.js
+++ test/cinema.test.js
@@ -14,5 +14,17 @@ describe('Check if all of the seats are avaliable for sale ', () => {
   //assert
   expect(allSeatsAvailable).toBe(true);
   });
+  test('check if return false if one seat is not available = 1 ', () => {
+    //arrange 
+    const dummyCinemaSeats = {
+      "A" : [0,0,0,0,0],
+      "B" : [0,0,1,0,0],
+      "c" : [0,0,0,0,0],
+    };
+    //act 
+    const allSeatsAvailable = isAllSeatsAVailable(dummyCinemaSeats);
+    //assert
+    expect(allSeatsAvailable).toBe(false);
+    });
 });
 
